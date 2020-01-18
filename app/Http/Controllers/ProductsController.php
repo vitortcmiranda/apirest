c6236cc5933b218b05ebfbff8c653794d4c1a911
Vitor Miranda <vitortcmout@gmail.com>
2020-01-18 17:28:33 -0300

    Model e Controller
    Criando a model e o controller

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -10,4 +10,26 @@ class ProductsController extends Controller
     public function index(){
         return Product::all();
     }
+
+    public function store(Request $request){
+        return Product::create($request->all());
+    }
+
+    public function excluir(Request $request){
+        Product::delete($request->where($request->id));
+        return 'Produto'.$request->title.' foi deletado';
+    }
+
+    public function update(Request $request,Prodcut $prodcut){
+        $product->update($request0>all());
+        return $product;
+    }
+
+    public function listar($id,Product $produto){
+        Product::withoutGlobalScope('id');
+        //$produto->where('id','=',$id);
+        dd($id);
+        return 'produto aí o infeliz'.$produto;
+        // return Product::selectOne()->whereRaw($id);
+    }
 }
--- app/Product.php
+++ app/Product.php
@@ -7,5 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     //
-    
+    public $timestamps = false;
+    protected $fillable = [
+        'title',
+        'body',
+        ];
 }
